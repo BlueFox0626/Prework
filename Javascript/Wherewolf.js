3f93218c2ae6d8ed5316b51411c024c5c5c5f840
Matilda Foster <dr.foster19@gmail.com>
2017-01-06 23:52:14 -0600

    Added h1 tag to css

---
+++ Javascript/Wherewolf.js
@@ -0,0 +1,28 @@
+var moonPhase = 'full';
+var foggyNight = false;
+
+if (moonPhase === 'full' || foggyNight) {
+  console.log('Howwwlll!!');
+} else if (moonPhase === 'mostly full') {
+  console.log('Arms and legs are getting hairier.');
+} else if (moonPhase === 'mostly new') {
+  console.log('Back on two feet');
+} else {
+  console.log('Invalid moon phase');
+}
+
+/*
+switch (moonPhase) {
+ case 'full':
+    console.log('Howwwwlll!!');
+    break;
+ case 'mostly full':
+    console.log('Arms and legs are getting hairier');
+    break;
+ case 'mostly new':
+    console.log('Back on two feet');
+    break;
+  default:
+    console.log('Invalid moon phase');
+    break;
+} */
